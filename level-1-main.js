e6d078adbf61865ba9f9207eb189af55e7466f0e
Tiffany J Brown <tiffany.brownjc@gmail.com>
2018-08-16 14:17:19 -0400

    added recipe objects

--- level-1-main.js
+++ level-1-main.js
@@ -8,6 +8,11 @@
 //     eyeColor:"blue"
 // };
 
+// const recipes = ["Guacamole", "Carrot Cake", "Hummus"]
+
+// The syntax for accessing the property of an object is:
+// objectName.property          // person.age
+
 const guacamole = {
     ingOne: "Avocado",
     ingTwo: "lime juice",
@@ -15,25 +20,51 @@ const guacamole = {
     ingFour: "onion", 
     ingFive: "tomato", 
     ingSix: "salt & pepper",
+    clue: "A dip or condiment originating in Mexico ",
 }
+console.log(guacamole.ingOne);
 
-const carrotCake = {
-    ingOne: "carrots",
-    ingTwo: "sugar",
-    ingThree: "eggs",
-    ingFour: "cinnamon", 
-    ingFive: "cream cheese", 
-    ingSix: "vannila", 
-    ingSeven: "walnuts",
-}
+// const carrotCake = {
+//     ingOne: "carrots",
+//     ingTwo: "sugar",
+//     ingThree: "eggs",
+//     ingFour: "cinnamon", 
+//     ingFive: "cream cheese", 
+//     ingSix: "vannila", 
+//     ingSeven: "walnuts",
+//     clue: "A cake made from a veggie for healthy eyes ",
+// }
+// console.log();
 
-const hummus = {
-    ingOne: "chickpeas",
-    ingTwo: "tahini",
-    ingThree: "lemon juice",
-    ingFour: "garlic",
-    ingFive: "salt and pepper",
-    ingSix: "olive oil",
-}
+// const hummus = {
+//     ingOne: "chickpeas",
+//     ingTwo: "tahini",
+//     ingThree: "lemon juice",
+//     ingFour: "garlic",
+//     ingFive: "salt and pepper",
+//     ingSix: "olive oil",
+//     clue: "Levantine dip or spread for veggies or pita",
+// }
+// console.log();
+
+// alert the general clue to each recipe
+
+
+
+
+
 
 // randomize recipes and ingredients
+// example:
+// var things = ['Rock', 'Paper', 'Scissor'];
+// var thing = things[Math.floor(Math.random()*things.length)];
+// alert('The computer chose:' + thing);
+
+// const recipes = ['Guacamole', 'Carrot Cake', 'Hummus'];
+// const recipe = recipe[Math.floor(Math.randon() * recipes.length)];
+
+// return('')
+
+
+
+//
