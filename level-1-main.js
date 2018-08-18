50fb819721470a2f839a19ece190b581c29b63fc
Tiffany J Brown <tiffany.brownjc@gmail.com>
2018-08-18 16:48:48 -0400

    added background, event listener, check answer

--- level-1-main.js
+++ level-1-main.js
@@ -1,32 +1,50 @@
 let body = document.querySelector('body')
 let h4 = document.querySelector('h4')
 let p = document.querySelector('p')
+let h6 = document.querySelector('h6')
 const recipes = [{
     name: 'Guacamole',
     ingredients: ['Avocado', 'Lime', 'Cilantro', 'Onion', 'Tomato', 'Salt and Pepper'],
-    clue: 'A dip or condiment originating in Mexico'
+    clue: 'A dip or condiment originating in Mexico' 
 },
 {
     name: 'Carrot Cake',
     ingredients: ['Carrots', 'Sugar', 'Eggs', 'Cinnamon', 'Flour', 'Cream Cheese'],
-    clue: 'A cake made from a veggie for healthy eyes'
+    clue: 'A cake made from a veggie, for healthy eyes'
 },
 {
     name: 'Hummus',
     ingredients: ['Chickpeas', 'Tahini', 'Lemon', 'Garlic', 'Salt and Pepper', 'Olive Oil'],
-    clue: 'Levantine dip or spread for veggies or pita'
+    clue: 'Levantine dip or spread for veggies or pita' 
 }];
 
 console.log(recipes);
 
-// CHOOSE RECIPE!!!!!!@!!
+// CHOOSE RECIPE!!!!!!!!
 let chosenRecipe = 0;
 let timesNumber = Math.floor(Math.random() * recipes.length)
 function randomRecipes(recipes) {
     chosenRecipe = recipes[timesNumber];
     return chosenRecipe;
 }
-randomRecipes(recipes)
+let currentQuestion = randomRecipes(recipes);
+console.log(currentQuestion);
+let submit = document.querySelector('#submit')
+//before we add event listener we instanciated submit value
+submit.addEventListener('click', function(event) {
+    event.preventDefault();
+   let input = document.querySelector('#input')
+//    console.log(input.value);
+checkAnswer();
+})
+function checkAnswer() {
+    if(currentQuestion.name === input.value){
+        alert("CORRECT!!!!!!");
+    }else{
+        alert("WRONG!!!");
+    }
+}
+
 
 console.log(chosenRecipe);
 
@@ -35,7 +53,7 @@ function createClue() {
     let alertClue = document.createElement('div')
     alertClue.classList.add('randomClue')
     alertClue.innerHTML = recipes[timesNumber].clue
-    h4.appendChild(alertClue)
+    h6.appendChild(alertClue)
 }
 createClue()
 
